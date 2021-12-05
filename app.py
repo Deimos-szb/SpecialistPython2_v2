fd0b4d9b741aea7618b31fb0cffb980406226c51
Deimos-szb <81982997+Deimos-szb@users.noreply.github.com>
2021-12-05 14:46:12 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, abort
+import random
+
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+def row_maker(quote):
+    return f"Quote {quote['id']}: {quote['text']} {quote['author']} (c)"
+
+quotes = [
+   {
+       "id": 1,
+       "author": "Rick Cook",
+       "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+   },
+   {
+       "id": 2,
+       "author": "Waldi Ravens",
+       "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+   },
+   {
+       "id": 3,
+       "author": "Mosher’s Law of Software Engineering",
+       "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+   },
+   {
+       "id": 4,
+       "author": "Yoggi Berra",
+       "text": "В теории, теория и практика неразделимы. На практике это не так."
+   },
+
+]
+
+
+@app.route("/")
+def hello_world():
+   return "Hello, World!"
+
+about_me = {
+   "name": "Евгений",
+   "surname": "Юрченко",
+   "email": "eyurchenko@specialist.ru"
+}
+
+@app.route('/quotes/<int:quote_id>')
+def show_quote(quote_id):
+    for quote in quotes:
+        if quote["id"] == quote_id:
+            return row_maker(quote)
+    abort(404)
+
+@app.route("/quotes/")
+@app.route("/quotes")
+def show_all_quotes():
+    return ' '.join([row_maker(quote) for quote in quotes])
+
+@app.route("/quotes/count")
+def show_num_of_qoutes():
+    return {"number": len(quotes)}
+
+@app.route("/quotes/random")
+def show_random_quote():
+    quote = random.choice(quotes)
+    return row_maker(quote)
+
+@app.route("/about")
+def about():
+   return about_me
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
